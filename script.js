9c0e675dac156ef7aa49eb0771b3c3ec9a0cebf8
sunayanapamula-gif <sunayanapamula@gmail.com>
2026-04-08 13:47:47 +0530

    Update script.js

--- script.js
+++ script.js
@@ -75,6 +75,16 @@ function createFish(type, bigTail=false, small=false) {
     fish.style.left = newLeft;
     setTimeout(() => fish.classList.remove('escape'), 1000);
   });
+  // Continuous random swimming
+  setInterval(() => {
+    const newTop = `${Math.random() * 70}vh`;
+    const newLeft = `${Math.random() * 90}vw`;
+    fish.style.transition = "top 5s ease, left 5s ease";
+    fish.style.top = newTop;
+    fish.style.left = newLeft;
+  }, 6000); // every 6 seconds
+}
+
 }
 
 // Fishes
